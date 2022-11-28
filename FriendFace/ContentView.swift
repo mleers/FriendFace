5ba610f051fd9d44f09260369857cde6bca09a4d
mleers <mleers4@gmail.com>
2022-11-28 15:19:53 -0800

    Initial Commit

---
+++ FriendFace/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by mitch on 11/28/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,18 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by mitch on 11/28/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
